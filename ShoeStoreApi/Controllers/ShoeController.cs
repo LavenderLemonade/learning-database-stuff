f33b72f3a6f80bca0b66f516f499713bb272911f
Sam Elliott <soe1@uakron.edu>
2022-11-01 16:38:26 -0400

    yeah

---
+++ ShoeStoreApi/Controllers/ShoeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatabaseStuff;
+
+namespace ShoeStoreApi.Controllers
+{
+    [Route("shoes")]
+    [ApiController]
+    public class ShoeController : ControllerBase
+    {
+        private IShoeDao shoeDao;
+
+        public ShoeController(IShoeDao shoeDao)
+        {
+            this.shoeDao = shoeDao;
+        }
+
+        [HttpGet()]
+        public List<string> ListShoes()
+        {
+            return shoeDao.GetInventory();
+        }
+    }
+}
